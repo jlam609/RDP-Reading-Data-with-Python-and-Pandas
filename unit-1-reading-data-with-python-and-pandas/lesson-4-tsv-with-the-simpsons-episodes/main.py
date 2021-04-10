73ebb4d094a482a454435eef373f56bc860f30c6
jlam609 <jlam609@gmail.com>
2021-04-10 14:44:49 -0400

    finished lesson 4

--- unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/main.py
@@ -3,5 +3,8 @@ import pandas as pd
 
 col_names = ['Title', 'Air date', 'Production code', 'Season', 'Number in season',
              'Number in series', 'US viewers (million)', 'Views', 'IMDB rating']
-             
-                          
+select_cols = ['Title', 'Air date', 'Production code', 'IMDB rating']
+simpsons = pd.read_csv(
+    "/Users/jasonlam/Desktop/projects/datascience/RDP-Reading-Data-with-Python-and-Pandas/unit-1-reading-data-with-python-and-pandas/lesson-4-tsv-with-the-simpsons-episodes/files/simpsons-episodes.tsv",
+    sep='\t', names=col_names, skiprows=4, index_col='Production code', na_values='no_val', parse_dates=['Air date'], usecols=select_cols)
+print(simpsons.head())
