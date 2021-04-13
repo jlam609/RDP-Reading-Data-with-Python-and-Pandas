9017d2775cb38494d753ed40d0dc848324571b04
jlam609 <jlam609@gmail.com>
2021-04-13 14:23:32 -0400

    completed lesson6

--- unit-1-reading-data-with-python-and-pandas/lesson-6-reading-playstore-apps-excel-file/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-6-reading-playstore-apps-excel-file/main.py
@@ -1,4 +1,8 @@
 import numpy as np
 import pandas as pd
 
+colNames = ['App', 'Rating', 'Installs', 'Rating', 'Genres', 'Last_Updated']
 data_url = 'https://github.com/ine-rmotr-projects/project-files/files/4086772/playstore.xlsx'
+playstore_df = pd.read_excel(data_url, usecols=colNames, date_parser=['Last_Updated'])
+playstore_df = playstore_df.sort_values('Rating', ascending = False).head(25)
+print(playstore_df)
