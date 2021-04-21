28e345bf45fb61a16c989f9fe4f47cb32957fbdd
jlam609 <jlam609@gmail.com>
2021-04-21 16:53:00 -0400

    finished lsson9

--- unit-1-reading-data-with-python-and-pandas/lesson-9-parse-artists-json-file/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-9-parse-artists-json-file/main.py
@@ -1,3 +1,11 @@
 import numpy as np
 import pandas as pd
 
+artists = pd.read_json('files/artists.json')
+print(artists)
+
+artists.drop(['bio'], axis='columns', inplace=True)
+
+artists.set_index('name', inplace=True)
+
+artists.to_csv('artists.csv')
