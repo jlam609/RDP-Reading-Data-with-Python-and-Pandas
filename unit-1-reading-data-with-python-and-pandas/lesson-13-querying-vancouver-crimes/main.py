6298c191bbc972abdd49b19d7cae70d503bfcb3d
jlam609 <jlam609@gmail.com>
2021-04-26 12:23:17 -0400

    finished 14

--- unit-1-reading-data-with-python-and-pandas/lesson-13-querying-vancouver-crimes/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-13-querying-vancouver-crimes/main.py
@@ -9,6 +9,13 @@ conn = sqlite3.connect(':memory:')
 c = conn.cursor()
 
 # restore the given van_crime_2003.sql dump
-c.executescript(open('van_crime_2003.sql', 'r').read())
+c.executescript(open('files/van_crime_2003.sql', 'r').read())
+van_crimes_df = pd.read_sql('''
+                 SELECT TYPE, MONTH, DAY, NEIGHBOURHOOD
+                 FROM van_crimes
+                 WHERE neighbourhood in ('Stanley Park', 'West End')       
+                        ''', conn)
+crime_types_count = van_crimes_df['TYPE'].value_counts()
+print(crime_types_count)
 
 # your code goes here
