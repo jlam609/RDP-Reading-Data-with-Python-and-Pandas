3f9493402e33703ac7aeb8906362c16d0f493ddb
jlam609 <jlam609@gmail.com>
2021-04-26 12:04:08 -0400

    finished 9 and 10

--- unit-1-reading-data-with-python-and-pandas/lesson-10-artists-nested-biography/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-10-artists-nested-biography/main.py
@@ -3,3 +3,14 @@ import pandas as pd
 import json
 from pandas.io.json import json_normalize
 
+with open('artists.json') as file:
+    json_dict = json.load(file)
+
+artists = pd.json_normalize(json_dict)
+
+biography = pd.json_normalize(json_dict,
+                           record_path='bio',
+                           meta=['name'])
+
+print(artists)
+print(biography)
